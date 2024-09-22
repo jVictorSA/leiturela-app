4aee00ebb63000919d7979c691b51beb914e7554
Gustavo Domingos <gdo@ic.ufal.br>
2024-09-22 15:16:55 -0300

    Corrigido problemas com a navegação e com a responsividade

--- lib/ui/minigames/minigames.dart
+++ lib/ui/minigames/minigames.dart
@@ -1,31 +1,41 @@
 import 'package:flutter/material.dart';
 import '../custom_widgets/return_button.dart';
+import '../games/activity_example/activity_example.dart';
 
-class Minigames extends StatelessWidget{
+class Minigames extends StatelessWidget {
   const Minigames({super.key});
 
   @override
-  Widget build(BuildContext context){    
-      return Scaffold(        
-        body: 
-        Container(
-          decoration: const BoxDecoration( 
-            image: DecorationImage(
-              image: AssetImage("assets/imgs/background.png"),
-              fit: BoxFit.contain,
-            ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/imgs/background.png"),
+            fit: BoxFit.contain,
           ),
-          child: Column(
-            children: [
-              Row(children: 
-                [ReturnButton(parentContext: context)]
-              ),
-              const Center(
-                child: Text("Tela de Minigames"),
-              )
-            ],        
-          )
         ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              child: ReturnButton(parentContext: context),
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ActivityExampleApp()),
+                  );
+                },
+                child: const Text("Mini game"),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
