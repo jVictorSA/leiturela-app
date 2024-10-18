397962b3676dfcecece47f9738d3abcf80740bf6
Gustavo Domingos <gdo@ic.ufal.br>
2024-10-18 01:13:09 -0300

    Reformula a tela de seleção de minigames

--- lib/ui/minigames/minigames.dart
+++ lib/ui/minigames/minigames.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import '../../main.dart';
 import '../custom_widgets/return_button.dart';
+import '../custom_widgets/selected_frame.dart';
+import '../games/activities/count_letters.dart';
 import '../games/activities/drag_crossword.dart';
 
 class Minigames extends StatelessWidget {
@@ -8,43 +12,101 @@ class Minigames extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/imgs/background.png"),
-            fit: BoxFit.contain,
-          ),
-        ),
+      body: SingleChildScrollView(
         child: Stack(
           children: [
-            Positioned(
-              top: 0,
-              left: 0,
-              child: ReturnButton(parentContext: context),
-            ),
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ActivityExampleApp()),
-                      );
-                    },
-                    child: const Text("Montar Palavra"),
-                  ),
-                  const SizedBox(width: 15),
-                  ElevatedButton(
-                    onPressed: () {
-                      null;
-                    },
-                    child: const Text("Letra Pelo Som"),
-                  ),
-                ],
+            Positioned.fill(
+              child: SvgPicture.asset(
+                "assets/imgs/background.svg", // Update with your SVG path
+                fit: BoxFit.cover, // Same as the fit you used for PNG
               ),
+            ),
+            Column(
+              children: [
+                Row(children: [ReturnButton(parentContext: context)]),
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SelectedFrame(
+                            parentContext: context,
+                            nextPage: const DragSyllables(),
+                            title: 'Montar Palavra',
+                            svgs: const [
+                              'assets/imgs/drag_and_drop.svg',
+                              'assets/imgs/build.svg',
+                              'assets/imgs/me.svg'
+                            ],
+                          ),
+                          SelectedFrame(
+                            parentContext: context,
+                            nextPage: const CountLetters(),
+                            title: 'Contar Letras',
+                            svgs: const [
+                              'assets/imgs/calculator.svg',
+                              'assets/imgs/a_capitalized.svg',
+                              'assets/imgs/three_hand.svg'
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SelectedFrame(
+                            parentContext: context,
+                            nextPage: const MyApp(),
+                            title: 'ATV 3',
+                            svgs: const [
+                              'assets/imgs/abc.svg',
+                              'assets/imgs/abc.svg',
+                              'assets/imgs/abc.svg'
+                            ],
+                          ),
+                          SelectedFrame(
+                            parentContext: context,
+                            nextPage: const MyApp(),
+                            title: 'ATV 4',
+                            svgs: const [
+                              'assets/imgs/abc.svg',
+                              'assets/imgs/abc.svg',
+                              'assets/imgs/abc.svg'
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SelectedFrame(
+                            parentContext: context,
+                            nextPage: const MyApp(),
+                            title: 'ATV 5',
+                            svgs: const [
+                              'assets/imgs/abc.svg',
+                              'assets/imgs/abc.svg',
+                              'assets/imgs/abc.svg'
+                            ],
+                          ),
+                          SelectedFrame(
+                            parentContext: context,
+                            nextPage: const MyApp(),
+                            title: 'ATV 6',
+                            svgs: const [
+                              'assets/imgs/abc.svg',
+                              'assets/imgs/abc.svg',
+                              'assets/imgs/abc.svg'
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
